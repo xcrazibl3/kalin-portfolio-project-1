122bcec4f5ab452e3f79613e10dd11e69291faa6
xcrazibl3 <xcrazibl3@gmail.com>
2025-07-10 17:57:41 +0300

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 
 const App = () => {
   return (
     <main className='min-h-dvh w-screen'>
       <Hero />
+      <About />
       <section className='bg-blue-75 h-dvh'></section>
     </main>
   );
