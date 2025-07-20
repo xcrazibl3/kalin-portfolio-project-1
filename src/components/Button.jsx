362050abeb66d10da48e1004351e4298f1479856
xcrazibl3 <xcrazibl3@gmail.com>
2025-07-20 17:32:47 +0300

    Implemented Rotate and Hover effect on the Bento Card and added glow effect on hover on the Button element with glow prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,22 @@
 import clsx from "clsx";
+import { useState } from "react";
 
-const Button = ({ id, title, rightIcon, leftIcon, containerClass }) => {
+const Button = ({
+  id,
+  title,
+  rightIcon,
+  leftIcon,
+  containerClass,
+  glow = false,
+}) => {
+  const [isShadowVisible, setIsShadowVisible] = useState(glow);
+  const [mouseCoords, setMouseCoords] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    setMouseCoords({ x: e.clientX - rect.left, y: e.clientY - rect.top });
+  };
+
   return (
     <button
       id={id}
@@ -8,10 +24,13 @@ const Button = ({ id, title, rightIcon, leftIcon, containerClass }) => {
         "group relative z-10 w-fit cursor-pointer overflow-hidden rounded-full bg-violet-50 px-7 py-3 text-black",
         containerClass
       )}
+      onMouseMove={(e) => handleMouseMove(e)}
+      onMouseEnter={() => setIsShadowVisible(true)}
+      onMouseLeave={() => setIsShadowVisible(false)}
     >
       {leftIcon}
 
-      <span className='relative inline-flex overflow-hidden font-general text-xs uppercase'>
+      <span className='relative inline-flex overflow-hidden font-general text-xs uppercase z-20'>
         <div className='translate-y-0 skew-y-0 transition duration-500 group-hover:translate-y-[-160%] group-hover:skew-y-12'>
           {title}
         </div>
@@ -21,6 +40,16 @@ const Button = ({ id, title, rightIcon, leftIcon, containerClass }) => {
       </span>
 
       {rightIcon}
+
+      {isShadowVisible && (
+        <div
+          className='absolute -translate-x-1/2 -translate-y-1/2 w-28 h-20 rounded-full bg-radial from-violet-400/40 to-transparent blur-lg'
+          style={{
+            top: `${mouseCoords.y}px`,
+            left: `${mouseCoords.x}px`,
+          }}
+        ></div>
+      )}
     </button>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -26,7 +26,34 @@ export default {
           300: "#EDFF66",
         },
       },
+      backgroundImage: {
+        radial: "radial-gradient(var(--tw-gradient-stops))",
+      },
+      perspective: {
+        100: "100px",
+        200: "200px",
+        300: "300px",
+        400: "400px",
+        500: "500px",
+        600: "600px",
+        700: "700px",
+        800: "800px",
+        900: "900px",
+        1000: "1000px",
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities, theme }) {
+      const values = theme("perspective");
+      const newUtilities = Object.entries(values).reduce(
+        (acc, [key, value]) => {
+          acc[`.perspective-${key}`] = { perspective: value };
+          return acc;
+        },
+        {}
+      );
+      addUtilities(newUtilities, ["responsive"]);
+    },
+  ],
 };
