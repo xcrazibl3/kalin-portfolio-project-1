1f7f3cb6fac5e49c31d61b636c0a084ec4d5b16e
xcrazibl3 <xcrazibl3@gmail.com>
2025-07-26 17:52:36 +0300

    Implemented the animated-title made it reusable with changing class selector to ref selector on the gsap animation also added titleColor prop

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,10 +1,22 @@
+import AnimatedTitle from "./AnimatedTitle";
+
 const Story = () => {
   return (
-    <section>
-      Story To implement Rotate Element like one in the bento cards so it could
-      be reusable. To extract it and use it as a wrapper
+    <section className='bg-black min-h-screen w-screen py-40'>
+      <div className='flex flex-col items-center'>
+        <p className='font-general uppercase text-white text-sm md:text-[10px]'>
+          the multiversal ip world
+        </p>
+        <AnimatedTitle
+          title='the st<b>o</b>ry of<br/> a hidden real<b>m</b>'
+          titleColor='text-white'
+          containerStyles='mt-5'
+        />
+      </div>
     </section>
   );
 };
 
 export default Story;
+
+// Where realms converge, lies Zentry and the boundless pillar. Discover its secrets and shape your fate amidst infinite opportunities.
