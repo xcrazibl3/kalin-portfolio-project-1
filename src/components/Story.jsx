0512a1bef90e75f03ea053651db2590771ba26b2
xcrazibl3 <xcrazibl3@gmail.com>
2025-07-27 13:31:40 +0300

    Finished the Story section

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -1,17 +1,18 @@
 import AnimatedTitle from "./AnimatedTitle";
 import TiltWrapper from "./TiltWrapper";
 import SVGMask from "./SVGMask";
+import Button from "./Button";
 
 const Story = () => {
   return (
     <section className='bg-black min-h-screen w-screen py-40'>
-      <div className='flex flex-col items-center'>
-        <p className='font-general uppercase text-white text-sm md:text-[10px]'>
+      <div className='flex flex-col items-center text-violet-50'>
+        <p className='font-general uppercase  text-sm md:text-[10px]'>
           the multiversal ip world
         </p>
         <AnimatedTitle
           title='the st<b>o</b>ry of<br/> a hidden real<b>m</b>'
-          titleColor='text-white'
+          titleColor='text-violet-50'
           containerStyles='mt-5 mix-blend-difference z-20'
         />
         {/* Image container */}
@@ -25,11 +26,17 @@ const Story = () => {
           />
           <SVGMask />
         </TiltWrapper>
+        <div className='md:self-end px-10 -mt-20 md:mt-0 flex-center flex-col'>
+          <p className='max-w-sm text-center font-circular-web md:text-start'>
+            Where realms converge, lies Zentry and the boundless pillar.
+            Discover its secrets and shape your fate amidst infinite
+            opportunities.
+          </p>
+          <Button title='discover prologue' containerClass='mt-6 text-xs' />
+        </div>
       </div>
     </section>
   );
 };
 
 export default Story;
-
-// Where realms converge, lies Zentry and the boundless pillar. Discover its secrets and shape your fate amidst infinite opportunities.
