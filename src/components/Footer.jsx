bfd335145563bc3e4a34d60b0bff4844bc4a59fb
xcrazibl3 <xcrazibl3@gmail.com>
2025-07-28 10:28:18 +0300

    Implemented the footer and styles for it

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,39 @@
+import {
+  FaFacebook,
+  FaInstagramSquare,
+  FaLinkedin,
+  FaGithub,
+} from "react-icons/fa";
+
 const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer className='h-20 w-screen bg-violet-300'>
-      <div>©Kalin Mihaylov {year}. All rights reserved</div>
-      <div></div>
-      <div></div>
+    <footer className='md:h-12 p-4 w-screen bg-violet-300 flex md:flex-row flex-col gap-y-4 justify-between items-center'>
+      <div>
+        <p className='text-sm font-light'>
+          ©Kalin Mihaylov {year}. All rights reserved
+        </p>
+      </div>
+      <div className='flex gap-x-2'>
+        <a href='https://www.facebook.com/kalin.mihaylov.31'>
+          <FaFacebook className='size-4 cursor-pointer hover:text-blue-50 duration-500' />
+        </a>
+        <a href='https://www.instagram.com/kala7aa/'>
+          <FaInstagramSquare className='size-4 cursor-pointer hover:text-blue-50 duration-500' />
+        </a>
+        <a href='https://www.linkedin.com/in/kalin-mihaylov-5334452ba/'>
+          <FaLinkedin className='size-4 cursor-pointer hover:text-blue-50 duration-500' />
+        </a>
+        <a href='https://github.com/xcrazibl3/kalin-portfolio-project-1'>
+          <FaGithub className='size-4 cursor-pointer hover:text-blue-50 duration-500' />
+        </a>
+      </div>
+      <div className='text-sm font-light group mr-4'>
+        <a className='group-hover:underline' href='#hero'>
+          Privacy Policy
+        </a>
+      </div>
     </footer>
   );
 };
